9e6c37aa4953d5d1ad34589fc4307d133f887b79
osmandgn <oskan001@gmail.com>
2023-01-20 20:25:58 +0300

    V1

--- src/main/java/com/tpe/security/service/WebSecurityConfig.java
+++ src/main/java/com/tpe/security/service/WebSecurityConfig.java
@@ -45,5 +45,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider());
+    }
+
+
 
 }
